4280eaa145eb608f11e46943bd708d8cb268b26d
Pedro Zulian <pedrotzf@gmail.com>
2020-06-02 18:10:12 -0300

    scrapy generate json yield with author and text

--- crawlers/author_spider.py
+++ crawlers/author_spider.py
@@ -1,22 +1,17 @@
 import scrapy
 
+
 class AuthorSpider(scrapy.Spider):
     name = 'author'
-    start_urls = ['http://quotes.toscrape.com/']
+    start_urls = ['http://quotes.toscrape.com/tag/humor/']
 
     def parse(self, response):
-        author_page_links = response.css('.author + a')
-        yield from response.follow_all(author_page_links, self.parse_author)
+        for quote in response.css('div.quote'):
+            yield {
+                'author': quote.xpath('span/small/text()').get(),
+                'text': quote.css('span.text::text').get(),
+            }
 
-        pagination_links = response.css('li.next a')
-        yield from response.follow_all(pagination_links, self.parse)
-
-    def parse_author(self, response):
-        def extract_with_css(query):
-            return response.css(query).get(default='').strip()
-
-        yield {
-            'name' : extract_with_css('h3.author-title::text'),
-            'birthdate' : extract_with_css('.author-born-date:text'),
-            'bio' : extract_with_css('.author-description:text')
-        }
+        next_page = response.css('li.next a::attr("href")').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
